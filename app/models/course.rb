4470b00cb64803ff8edea0a37aed0473a2205cf4
rystills <rystills@gmail.com>
2017-04-30 20:46:53 +0000

    exposed port in docker-compose, fixed sunspot config ports, re-added searchable block to course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,10 @@ class Course < ActiveRecord::Base
   validates  :number, presence: true, uniqueness: { scope: :department_id }
   default_scope { order(number: :asc) }
 
+  searchable do #searchable block required by sunspot
+    text :number, :name, :description
+  end
+
   def self.get code, number
     joins(:department).where("departments.code = ? AND number = ?", code, number).first
   end
@@ -13,7 +17,6 @@ class Course < ActiveRecord::Base
         courses = Sunspot.search(Course) do 
         fulltext params 
     end.results
-    courses = find_by_sql(query).uniq
     ActiveRecord::Associations::Preloader.new.preload(courses, :sections)
     courses
   end
